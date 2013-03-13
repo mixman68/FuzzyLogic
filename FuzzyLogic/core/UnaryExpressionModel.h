061a85fb77c5786fb861f6b7f652a3313ee10508
djgreg83 <greg.djg13@gmail.com>
2013-03-13 11:23:27 +0100

    Added Getters and Setters for UnaryExpressionModel.h

--- FuzzyLogic/core/UnaryExpressionModel.h
+++ FuzzyLogic/core/UnaryExpressionModel.h
@@ -11,6 +11,8 @@ namespace core
         public:
             virtual T Evaluate() const;
             virtual T Evaluate(Expression<T>*) const;
+            virtual void SetOperareur(UnaryExpression<T>*);
+            virtual void SetOperand(Expression<T>*);
         protected:
         private:
             UnaryExpression<T>* operateur;
@@ -30,8 +32,20 @@ namespace core
     T UnaryExpressionModel<T>::Evaluate() const
     {
         if(operand !=NULL)
-            throw (NullExpressionException("operator missing"));
+            throw (NullExpressionException("operand missing"));
         return Evaluate(&operand);
+    }
+
+    template<class T>
+    void UnaryExpressionModel<T>::SetOperareur(UnaryExpression<T>* _operateur)
+    {
+        operateur=_operateur;
+    }
+
+    template<class T>
+    void UnaryExpressionModel<T>::SetOperand(Expression<T>* _operand)
+    {
+        operand=_operand;
     }
 
 }
