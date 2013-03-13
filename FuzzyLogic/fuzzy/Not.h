11b509dbbcfb6b70aebb77e5a7e16781215ed09d
djgreg83 <greg.djg13@gmail.com>
2013-03-13 11:28:20 +0100

    NotMinus1.cpp Not.cpp
    classes

---
+++ FuzzyLogic/fuzzy/Not.h
@@ -0,0 +1,22 @@
+#ifndef NOT_H
+#define NOT_H
+
+
+#include "../core/UnaryExpression.h"
+#include "../core/Expression.h"
+
+using namespace core;
+
+namespace fuzzy
+{
+    template <class T>
+    class Not: public UnaryExpression<T>
+    {
+        public:
+            virtual T Evaluate(Expression<T>*) const;
+        protected:
+        private:
+    };
+}
+
+#endif // NOT_H
---
+++ FuzzyLogic/fuzzy/NotMinus1.h
@@ -0,0 +1,25 @@
+#ifndef NOTMINUS1_H
+#define NOTMINUS1_H
+
+#include "Not.h"
+
+namespace fuzzy
+{
+    template <class T>
+    class NotMinus1: public Not<T>
+    {
+        public:
+            virtual T Evaluate(Expression<T>*) const;
+        protected:
+        private:
+    };
+
+    template <class T>
+    T NotMinus1<T>::Evaluate(Expression<T>* _operand) const
+    {
+        T operand = _operand->Evaluate();
+
+    }
+}
+
+#endif // NOTMINUS1_H
