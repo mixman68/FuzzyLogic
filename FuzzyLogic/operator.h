0142235223b52ad9741e4a14b86e5374378e0a2f
djgreg83 <greg.djg13@gmail.com>
2013-03-18 09:01:19 +0100

    Partial list of operator

---
+++ FuzzyLogic/operator.h
@@ -0,0 +1,25 @@
+#ifndef OPERATOR_H
+#define OPERATOR_H
+
+#include "fuzzy/Agg.h"
+#include "fuzzy/AggMax.h"
+#include "fuzzy/AggPlus.h"
+
+#include "fuzzy/And.h"
+#include "fuzzy/AndMin.h"
+#include "fuzzy/AndMult.h"
+
+#include "fuzzy/Not.h"
+
+
+#include "fuzzy/Or.h"
+
+
+#include "fuzzy/Then.h"
+
+
+#include "fuzzy/is.h"
+
+
+#endif
+
