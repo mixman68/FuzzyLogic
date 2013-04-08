14a101f888e9830976208261874f89ea8b4b3905
djgreg83 <greg.djg13@gmail.com>
2013-04-08 09:47:07 +0200

    FuzzyLogic/core/FuzzyFactory.h empty file

---
+++ FuzzyLogic/core/FuzzyFactory.h
@@ -0,0 +1,17 @@
+#ifndef FUZZYFACTORY_H
+#define FUZZYFACTORY_H
+
+#include "ExpressionFactory.h"
+
+namespace core
+{
+    template <class T>
+    class FuzzyFactory: public ExpressionFactory<T>
+    {
+        public:
+        protected:
+        private:
+    };
+}
+
+#endif // FUZZYFACTORY_H
