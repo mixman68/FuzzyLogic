86edc33545f5d72e72c5b2e667614b25164ab686
djgreg83 <greg.djg13@gmail.com>
2013-04-08 10:53:05 +0200

    FuzzyFactory.h empty file

--- FuzzyLogic/core/FuzzyFactory.h
+++ FuzzyLogic/core/FuzzyFactory.h
@@ -2,6 +2,8 @@
 #define FUZZYFACTORY_H
 
 #include "ExpressionFactory.h"
+#include "ValueModel.h"
+#include "../fuzzy/Is.h"
 
 namespace core
 {
@@ -9,6 +11,13 @@ namespace core
     class FuzzyFactory: public ExpressionFactory<T>
     {
         public:
+             Expression<T>* newAnd(Expression<T>*,Expression<T>*);
+             Expression<T>* newOr(Expression<T>*,Expression<T>*);
+             Expression<T>* newThen(Expression<T>*,Expression<T>*);
+             Expression<T>* newAgg(Expression<T>*,Expression<T>*);
+             Expression<T>* newDefyzz(Expression<T>*,Expression<T>*);
+             Expression<T>* newNot(Expression<T>*);
+             Expression<T>* newIs(fuzzy::is<T>*, Expression<T>*);
         protected:
         private:
     };
