4abeaba456727ccfc6e2ba27b26a8ea4e3851fa3
djgreg83 <greg.djg13@gmail.com>
2013-04-08 09:58:17 +0200

    ExpressionFactory.h fixes

--- FuzzyLogic/core/ExpressionFactory.h
+++ FuzzyLogic/core/ExpressionFactory.h
@@ -5,35 +5,38 @@
 #include "UnaryExpressionModel.h"
 #include "BinaryExpressionModel.h"
 
+#include <vector>
+
 namespace core
 {
     template <class T>
     class ExpressionFactory
     {
         public:
-            Expression<T> Hold(Expression<T>);
-            UnaryExpressionModel<T> newUnary(UnaryExpression<T>,Expression<T>);
-            BinaryExpressionModel<T>  newBinary(BinaryExpression<T>,Expression<T>,Expression<T>);
+            Expression<T> Hold(Expression<T>* );
+            UnaryExpressionModel<T> newUnary(UnaryExpression<T>*,Expression<T>*);
+            BinaryExpressionModel<T>  newBinary(BinaryExpression<T>*,Expression<T>*,Expression<T>*);
         protected:
         private:
+            std::vector<Expression<T>*> memory;
     };
 
 
     template<class T>
-    Expression<T> ExpressionFactory<T>::Hold(Expression<T> expr)
+    Expression<T> ExpressionFactory<T>::Hold(Expression<T>* expr)
     {
-        //todo
-        return NULL;
+        memory.push_back(expr);
+        return expr;
     }
 
     template<class T>
-    UnaryExpressionModel<T> ExpressionFactory<T>::newUnary(UnaryExpression<T> ope, Expression<T> o)
+    UnaryExpressionModel<T> ExpressionFactory<T>::newUnary(UnaryExpression<T>* ope, Expression<T>* o)
     {
         return new UnaryExpressionModel<T>(ope,o);
     }
 
     template<class T>
-    BinaryExpressionModel<T> ExpressionFactory<T>::newBinary(BinaryExpression<T> ope, Expression<T> l, Expression<T> r)
+    BinaryExpressionModel<T> ExpressionFactory<T>::newBinary(BinaryExpression<T>* ope, Expression<T>* l, Expression<T>* r)
     {
         return new BinaryExpressionModel<T>(ope,l,r);
     }
