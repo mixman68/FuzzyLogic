d903e3f83539be45fdccce1d0d4e184a7ceec166
djgreg83 <greg.djg13@gmail.com>
2013-04-08 11:28:57 +0200

    Fixes FuzzyFactory.h

--- FuzzyLogic/core/FuzzyFactory.h
+++ FuzzyLogic/core/FuzzyFactory.h
@@ -3,7 +3,11 @@
 
 #include "ExpressionFactory.h"
 #include "ValueModel.h"
-#include "../fuzzy/Is.h"
+#include "../operator.h"
+#include "../expr.h"
+#include "BinaryShadowExpression.h"
+#include "UnaryShadowExpression.h"
+
 
 namespace core
 {
@@ -15,12 +19,57 @@ namespace core
              Expression<T>* NewOr(Expression<T>*,Expression<T>*);
              Expression<T>* NewThen(Expression<T>*,Expression<T>*);
              Expression<T>* NewAgg(Expression<T>*,Expression<T>*);
-             Expression<T>* NewDefyzz(Expression<T>*,Expression<T>*);
+             Expression<T>* NewDefuzz(Expression<T>*,Expression<T>*);
              Expression<T>* NewNot(Expression<T>*);
              Expression<T>* NewIs(fuzzy::is<T>*, Expression<T>*);
         protected:
         private:
+            BinaryShadowExpression<T>* cul;
+            UnaryShadowExpression<T>* non;
     };
+
+
+    template <class T>
+    Expression<T>* FuzzyFactory<T>::NewAnd(Expression<T>* l,Expression<T>* r)
+    {
+
+    }
+
+    template <class T>
+    Expression<T>* FuzzyFactory<T>::NewOr(Expression<T>* l,Expression<T>* r)
+    {
+
+    }
+
+    template <class T>
+    Expression<T>* FuzzyFactory<T>::NewThen(Expression<T>* l,Expression<T>* r)
+    {
+
+    }
+
+    template <class T>
+    Expression<T>* FuzzyFactory<T>::NewAgg(Expression<T>* l,Expression<T>* r)
+    {
+
+    }
+
+    template <class T>
+    Expression<T>* FuzzyFactory<T>::NewDefuzz(Expression<T>* l,Expression<T>* r)
+    {
+
+    }
+
+    template <class T>
+    Expression<T>* FuzzyFactory<T>::NewNot(Expression<T>* expr)
+    {
+
+    }
+
+    template <class T>
+    Expression<T>* FuzzyFactory<T>::NewIs(fuzzy::is<T>* is, Expression<T>* e)
+    {
+
+    }
 }
 
 #endif // FUZZYFACTORY_H
