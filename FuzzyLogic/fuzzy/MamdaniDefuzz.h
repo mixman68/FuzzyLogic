3120824170e0141933eeca3abfedb8a0591780e2
djgreg83 <greg.djg13@gmail.com>
2013-05-10 21:06:46 +0200

    Defuzzy Mamdani OK ;)

--- FuzzyLogic/fuzzy/MamdaniDefuzz.h
+++ FuzzyLogic/fuzzy/MamdaniDefuzz.h
@@ -3,6 +3,7 @@
 
 #include "../core/BinaryExpression.h"
 #include "../core/Expression.h"
+#include "../core/Shape.h"
 
 using namespace core;
 
@@ -12,17 +13,25 @@ namespace fuzzy
     class MamdaniDefuzz: public BinaryExpression<T>
     {
         public:
-            virtual T Evaluate(Expression<T>*,Expression<T>*) const;
+            MamdaniDefuzz();
+            MamdaniDefuzz(const T&,const T&,const T&);
+            virtual T Evaluate(Expression<T>*,Expression<T>*) const = 0;
         protected:
+            T mini, maxi, step;
         private:
     };
-    template <class T>
-    T MamdaniDefuzz<T>::Evaluate(Expression<T>* left,Expression<T>* right) const
-    {
-        T l = left->Evaluate();
-        T r = right->Evaluate();
 
-    }
+    template<class T>
+	MamdaniDefuzz<T>::MamdaniDefuzz():
+	mini(0),maxi(0),step(0)
+	{
+	}
+
+	template<class T>
+	MamdaniDefuzz<T>::MamdaniDefuzz(const T& _min, const T& _max, const T& _step):
+	mini(_min),maxi(_max),step(_step)
+	{
+	}
 
 }
 
