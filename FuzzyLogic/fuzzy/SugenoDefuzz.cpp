89e5cd58e3827fc6a7d86ac29b24d0f99610bcc3
djgreg83 <greg.djg13@gmail.com>
2013-06-01 17:57:51 +0200

    SugenoDefuzz

---
+++ FuzzyLogic/fuzzy/SugenoDefuzz.cpp
@@ -0,0 +1,1 @@
+#include "SugenoDefuzz.h"
---
+++ FuzzyLogic/fuzzy/SugenoDefuzz.h
@@ -0,0 +1,58 @@
+#ifndef SUGENODEFUZZ_H
+#define SUGENODEFUZZ_H
+
+#include "../core/NaryExpression.h"
+#include "../expr.h"
+
+#include "SugenoThen.h"
+
+using namespace core;
+using namespace std;
+
+namespace fuzzy
+{
+template <class T>
+class SugenoDefuzz: public NaryExpression<T>
+{
+public:
+    SugenoDefuzz();
+    virtual T Evaluate(vector<const Expression<T>*>*) const;
+protected:
+private:
+};
+
+template <class T>
+T SugenoDefuzz<T>::Evaluate(vector<const Expression<T>*>* operands) const
+{
+    typename vector<const Expression<T>*>::const_iterator oItr ;
+    T num = 0;
+    T denum = 0;
+
+    //on evalue tous les operandes
+
+    for(oItr=operands->begin(); oItr!=operands->end(); ++oItr)
+    {
+        BinaryExpressionModel<T> *operand = (BinaryExpressionModel<T>*) (*oItr);
+        //onprend osn operateur
+        BinaryShadowExpression<T> *operateur = (BinaryShadowExpression<T>*) operand->GetOperateur();
+
+        //Numérateur
+        num+=operand->Evaluate();
+
+        //Dénominateur
+
+        SugenoThen<T> *then = (SugenoThen<T>*) operateur->GetTarget();
+
+        denum += then->GetPremiseValue();
+    }
+
+    if(denum==0)
+        return 0;
+
+    T tmp = num/denum;
+
+    return tmp;
+}
+}
+
+#endif // SUGENODEFUZZ_H
