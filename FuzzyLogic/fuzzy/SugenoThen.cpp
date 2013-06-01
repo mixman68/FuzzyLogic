edc3e7979556a7ed919f2f9fb688fcfdd4ce2910
djgreg83 <greg.djg13@gmail.com>
2013-06-01 17:57:36 +0200

    SugenoThen

---
+++ FuzzyLogic/fuzzy/SugenoThen.cpp
@@ -0,0 +1,2 @@
+#include "SugenoThen.h"
+
---
+++ FuzzyLogic/fuzzy/SugenoThen.h
@@ -0,0 +1,45 @@
+#ifndef SUGENOTHEN_H
+#define SUGENOTHEN_H
+
+#include "Then.h"
+
+namespace fuzzy
+{
+template<class T>
+class SugenoThen: public Then<T>
+{
+public:
+    SugenoThen();
+    virtual ~SugenoThen() {};
+
+    virtual T Evaluate(Expression<T>*,Expression<T>*) const;
+
+    virtual T GetPremiseValue() const;
+protected:
+private:
+    mutable T premiseValue;
+};
+
+template<class T>
+SugenoThen<T>::SugenoThen(): premiseValue(0)
+{
+}
+
+template<class T>
+T SugenoThen<T>::GetPremiseValue() const
+{
+    return premiseValue;
+}
+
+template<class T>
+T SugenoThen<T>::Evaluate(core::Expression<T>* left, core::Expression<T>* right) const
+{
+    premiseValue = left->evaluate();
+    T tmp = premiseValue * right->evaluate();
+
+    return tmp;
+}
+}
+
+
+#endif // SUGENOTHEN_H
